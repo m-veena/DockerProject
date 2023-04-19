f6b6e786fcdaa5b73ef32b6a414900caa19d2ee2
madhu veena <madhu.veena123@gmail.com>
2023-04-19 15:13:57 +1000

    updated

--- src/main/java/test/docker/TestDocker.java
+++ src/main/java/test/docker/TestDocker.java
@@ -2,7 +2,7 @@ package test.docker;
 
 public class TestDocker {
 	public static void main(String[] args) {
-		System.out.println("Docker Test");
+		System.out.println("Docker Test program running");
 	}
 
 }
